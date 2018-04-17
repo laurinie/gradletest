e761d69284749ae052541b07e7c82a7c36516adf
laurinie <niemi.late@gmail.com>
2018-04-17 11:03:43 +0300

    failing unit test

---
+++ src/test/java/org/gradle/demo/TestApp.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gradle.demo;
+
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author niemi
+ */
+public class TestApp {
+    
+//    public TestApp() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void dummyUnitTest() {
+        assertEquals(0, 1);
+    }
+    @Test
+    public void dummyUnitTestPass() {
+        assertEquals(0, 0);
+    }
+
+}
